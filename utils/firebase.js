5bcc890be523928523988f07ec443e1ec2744325
WorstDudeOfAllTime <Krisbertolino@gmail.com>
2022-10-04 01:07:44 -0400

    revert

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_DB_APIKEY,
+  apiKey: "AIzaSyCI4QNqoU5I_D8iBl43b-F3DXLeL1yw_D4",
   authDomain: "restauranti-30e36.firebaseapp.com",
   databaseURL: "https://restauranti-30e36-default-rtdb.firebaseio.com",
   projectId: "restauranti-30e36",
   storageBucket: "restauranti-30e36.appspot.com",
   messagingSenderId: "986340391104",
-  appId: process.env.NEXT_PUBLIC_DB_APPID,
+  appId: "1:986340391104:web:29f171d3ad12f50c8fd525",
 };
 const firebaseApp = initializeApp(firebaseConfig);
 const database = getFirestore(firebaseApp);
